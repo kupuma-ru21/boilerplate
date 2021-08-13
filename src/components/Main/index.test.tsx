c8c194c93686e11b3fa191a708483025d1ea7d4a
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-14 01:53:10 +0900

    feat: add Styles to our Main component

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -4,8 +4,15 @@ import Main from '.';
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />);
-    screen.getByRole('heading', { name: 'React Advance' });
+    expect(
+      screen.getByRole('heading', { name: /React Advance/i })
+    ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
   });
 });
