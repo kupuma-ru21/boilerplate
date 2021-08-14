07abe3e5ed4d97bcbd90b78945a0daf0dd6e16f2
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-14 13:26:19 +0900

    feat: update to use Storybook Essentials e controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,21 +1,21 @@
 import type { Meta, Story } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
+  args: { title: 'title default', description: 'description default' },
 } as Meta;
 
-export const Basic: Story = () => {
-  return (
-    <Main
-      title={text('Title', 'React Advance')}
-      description={text(
-        'Description',
-        'TypeScript, ReactJS, NextJS e Styled Components'
-      )}
-    />
-  );
+export const Basic: Story = (args) => {
+  return <Main {...args} />;
+};
+
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic',
+};
+
+export const Default: Story = (args) => {
+  return <Main {...args} />;
 };
